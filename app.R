90b1acdfa1023bf913af71ebf8cd3871d4f4d09a
Casey Lum <caseylum@uw.edu>
2017-03-01 16:51:18 -0800

    Upload starter files

---
+++ app.R
@@ -0,0 +1,7 @@
+library(dplyr)
+library(ggplot2)
+library(plotly)
+library(shiny)
+
+
+shinyApp(ui=shinyUI(ui), server=shinyServer(server))
---
+++ ui.R
@@ -0,0 +1,10 @@
+library(dplyr)
+library(ggplot2)
+library(plotly)
+library(shiny)
+
+
+ui <- fluidPage(
+  titlePanel("Title"),
+  h3(em("By Kelsey Kua, Casey Lum, and Devin Reich"))
+)
