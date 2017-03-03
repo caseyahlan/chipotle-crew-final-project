665b865d753bce5b1458c321270fd143688ef317
Kelsey Kua <kelsey.kua@gmail.com>
2017-03-03 15:45:49 -0800

    Start working on server

---
+++ server.R
@@ -0,0 +1,14 @@
+library("shiny")
+library("ggplot2")
+library("dplyr")
+library("plotly")
+
+# Sets up API information
+base <- "https://api.spotify.com"
+
+
+
+
+server <- function(input, output) {
+  # Everything goes here
+}
